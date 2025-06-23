470d2d26ba6e46385528dca3e566c1ce5fcc5294
SeongUk Kim <td1118@yonsei.ac.kr>
2025-06-23 10:07:32 +0900

    Update SchedulerApplicationService.java

--- java/egovframework/sys/scheduler/SchedulerApplicationService.java
+++ java/egovframework/sys/scheduler/SchedulerApplicationService.java
@@ -6,10 +6,8 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 public interface SchedulerApplicationService {
 
-	//모집완료 캠페인 시작일시 -> 진행
 	void updateToProgressCampaign();
 	
-	//진행중인 캠페인 종료일시 -> 종료상태변환
 	void updateToEndCampaign();
 	
 	List<EgovMap> schedulerTest();
